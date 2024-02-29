f7ed7bfd88706f7dc8be0b06c992cf21c21420c2
danique <kiseldaniil25@gmail.com>
2024-03-01 01:39:33 +0300

    update

--- src/main/java/com/ip_position/ipPosition/IpPositionApplication.java
+++ src/main/java/com/ip_position/ipPosition/IpPositionApplication.java
@@ -3,13 +3,11 @@ package com.ip_position.ipPosition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class IpPositionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IpPositionApplication.class, args);
 	}
-
 
 }
