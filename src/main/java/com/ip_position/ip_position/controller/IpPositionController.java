74bdd2f1a9f51a0e26618dc84167e25b8e188c27
danique <kiseldaniil25@gmail.com>
2024-03-01 16:10:50 +0300

    update

--- src/main/java/com/ip_position/ip_position/controller/IpPositionController.java
+++ src/main/java/com/ip_position/ip_position/controller/IpPositionController.java
@@ -18,7 +18,7 @@ public class IpPositionController {
     }
 
     private boolean isValidIpAddress(String ip) {
-        return ip.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
+        return ip.matches("^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
     }
 
     @GetMapping("/{ip}")
