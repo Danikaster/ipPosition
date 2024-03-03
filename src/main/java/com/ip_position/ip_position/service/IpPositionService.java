98660f3c65a97d1c3334de192108f3777d69ecc4
danique <kiseldaniil25@gmail.com>
2024-03-04 02:28:01 +0300

    update

--- src/main/java/com/ip_position/ip_position/service/IpPositionService.java
+++ src/main/java/com/ip_position/ip_position/service/IpPositionService.java
@@ -3,15 +3,12 @@ package com.ip_position.ip_position.service;
 import com.ip_position.ip_position.model.IpPositionModel;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class IpPositionService {
 
     public IpPositionModel getIpInfo(String ip) {
         String apiUrl = "http://ipwho.is/" + ip;
-
-
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(apiUrl, IpPositionModel.class);
     }
